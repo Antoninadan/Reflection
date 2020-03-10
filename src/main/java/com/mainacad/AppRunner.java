0552c62aa5ed3c072b2c72c952c8b546f5a6fe80
Antoninadan <mail100@i.ua>
2020-03-10 22:58:28 +0200

    initial commit

---
+++ src/main/java/com/mainacad/AppRunner.java
@@ -0,0 +1,76 @@
+package main.java.com.mainacad;
+
+import main.java.com.mainacad.model.User;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppRunner {
+
+    public static void main(String[] args) {
+
+        User user = new User(1, "login123456", "pass", "fname", "lname");
+        User user2 = new User(1, "login123", "pass", "fname", "lname");
+
+
+        Field[] fields = user.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            System.out.println(field.getName());
+        }
+
+        Class superUserClass = user.getClass().getSuperclass();
+        Field[] sFields = superUserClass.getDeclaredFields();
+        for (Field field : sFields) {
+            System.out.println(field.getName());
+        }
+
+        User user3 = user.clone();
+
+
+        final String sql = "SELECT ...";
+
+        String sql2 = "INSERT ...";
+        sql2 += " SOMETHING";
+
+        final List<Integer> ids = new ArrayList<>();
+
+
+        List<Integer> ids2 = new ArrayList<>();
+
+        try {
+            Class<User> type = User.class;
+            Field field = type.getDeclaredField("login");
+            field.setAccessible(true);
+            try {
+                System.out.println(field.get(user));
+                System.out.println(field.get(user2));
+                System.out.println(field.get(user3));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+
+//            System.out.println(field);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        if (user.getClass().getName().equals("main.java.com.mainacad.model.User")) {
+            System.out.println("true");
+        }
+
+        if (user.getClass().getSimpleName().equals("User")) {
+            System.out.println("true");
+        }
+
+        if (user.getClass().equals(User.class)) {
+            System.out.println("true");
+        }
+
+        if (user instanceof User) {
+            System.out.println("true");
+        }
+
+
+    }
+}
---
+++ src/main/java/com/mainacad/model/User.java
@@ -0,0 +1,45 @@
+package main.java.com.mainacad.model;
+
+/**
+ * This is User class
+ *
+ */
+
+public class User extends BaseEntity implements Cloneable{
+    private String login;
+    private String password;
+    private String firstName;
+    private String secondName;
+
+    public User() {
+    }
+
+    /**
+     *
+     * @param id is set in super class
+     * @param login
+     * @param password
+     * @param firstName
+     * @param secondName
+     */
+    public User(Integer id, String login, String password, String firstName, String secondName) {
+        this.setId(id);
+        this.login = login;
+        this.password = password;
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    /**
+     *
+     * @return clone of {@code main.java.com.mainacad.model.User} inctance
+     */
+    public User clone(){
+        try {
+            return (User) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
